b3c900b73713f4b9e10b6348d598720ac5b75455
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-12-30 21:23:23 -0300

    ok

--- nodes/manager.py
+++ nodes/manager.py
@@ -1,1 +1,27 @@
 # Create here the NodeManager Class
+from typing import List
+from nodes.node import Node
+
+class NodeManager:
+    
+    nodes: List[Node]
+
+    def __init__(self, nodes: List[Node]):
+        self.nodes = nodes        
+        
+    def remove(self, nodes):
+        self.nodes = nodes
+        
+    def remove_cascade(self, children):
+        self.children = children
+    
+    def __len__(self):
+        return len(self.nodes)
+
+    def __getitem__(self, nodes):
+        return self.nodes[nodes]
+   
+
+
+    
+    
--- nodes/node.py
+++ nodes/node.py
@@ -1,1 +1,22 @@
 # Create here the Node Class
+
+class Node:
+    id: int
+    parent: int
+    def __init__(self, id: int, parent: int):
+        self.id = id
+        self.parent = parent
+        
+        if (int(id) == False):
+            raise ValueError
+        
+        if (id < parent):
+            raise ValueError
+
+        if (id == parent):
+            raise ValueError
+        
+    def __repr__(self):
+        if (Node != "( , )"):
+            return f"Node({self.id}, {self.parent})"  
+        
