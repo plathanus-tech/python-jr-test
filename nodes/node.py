2cd2979d68bce67afcc86316fb8452509df2d661
Mendelski <erick.mendelski@gmail.com>
2022-01-27 23:52:51 -0300

    [test/mendelski] Add node class and his validations

--- nodes/node.py
+++ nodes/node.py
@@ -1,1 +1,21 @@
-# Create here the Node Class
+def is_valid_value(id, parent) -> bool:
+    is_params_int = isinstance(id, int) or isinstance(parent, int)
+    is_id_greater = id > parent
+
+    if is_params_int and is_id_greater:
+        return True
+    else:
+        raise ValueError
+
+
+class Node:
+    id: int
+    parent: int
+
+    def __init__(self, id: int, parent: int):
+        if is_valid_value(id, parent):
+            self.id = id
+            self.parent = parent
+
+    def __repr__(self):
+        return f"Node({self.id}, {self.parent})"
