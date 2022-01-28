6d6bd5e9503d9a73a8f1b8420406a7612d1d7c2a
Mendelski <erick.mendelski@gmail.com>
2022-01-28 01:49:00 -0300

    [test/mendelski] working in progress

--- nodes/manager.py
+++ nodes/manager.py
@@ -1,1 +1,40 @@
-# Create here the NodeManager Class
+from typing import List
+
+from nodes.node import Node
+
+
+def is_valid_value(nodes) -> bool:
+    is_node_type = isinstance(nodes, List)
+    # is_sequence = ''
+
+    if is_node_type:
+        return True
+    else:
+        raise ValueError
+
+
+class NodeManager:
+    nodes: List[Node]
+
+    def __init__(self, nodes: List[Node]):
+        if is_valid_value(nodes):
+            self.nodes = nodes
+
+    def __len__(self):
+        return len(self.nodes)
+
+    def __getitem__(self, item):
+        return self.nodes[item]
+
+    def remove(self, node: Node):
+        return self.nodes.remove(node)
+
+    def remove_cascade(self, node: Node):
+        parent = set()
+
+        for n in self.nodes:
+            parent.add(n.parent)
+
+        for node in self.nodes:
+            if node.parent in parent:
+                self.remove(node)
