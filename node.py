53c46766d369930f1de061cea8fe6c0d8c8bfe9a
Gustavo Domingos Garcia <81938273+gdomingosg@users.noreply.github.com>
2022-02-09 19:16:49 -0300

    Update manager.py and node.py

---
+++ node.py
@@ -0,0 +1,29 @@
+# Create here the Node Class
+
+
+class Node:
+    id: int
+    parent: int
+
+    def __init__(self, id: int, parent: int):
+
+        if not isinstance(id, int):
+            raise ValueError("The node's id must to be an interger")
+        else:
+            self.id = id
+
+        if not isinstance(parent, int):
+            raise ValueError("The node's parent must to be an interger")
+        elif parent >= id:
+            raise ValueError("An Node parent can't be greater or equal to the node's id")
+        else:
+            self.parent = parent
+
+    def __repr__(self) -> str:
+        return f'Node({self.id}, {self.parent})'
+
+    def id_num(self) -> int:
+        return self.id
+
+    def parent_num(self) -> int:
+        return self.parent
