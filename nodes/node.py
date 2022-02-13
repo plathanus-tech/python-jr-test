2203e8bd07e2f51523cda33e7b3ab33a31d8543d
Victoria Fabris <vicfabriss@gmail.com>
2022-02-13 18:18:16 -0300

    Implements Node Class and add requirement.txt

--- nodes/node.py
+++ nodes/node.py
@@ -1,5 +1,21 @@
-# Create here the Node Class
-
 
 class Node:
-    pass
+    def __init__(self, id: int, parent: int):
+        if (type(id) != int) or (type(parent) != int):
+            raise ValueError("Document Id and Document Parent should be integers!")
+        elif parent >= id:
+            raise ValueError("Parent can't be equal or greater than id!")
+
+        else:
+            self.id: int = id
+            self.parent: int = parent
+
+    def __repr__(self) -> str:
+        return "Node({},{})".format(self.id, self.parent)
+
+if __name__ == "__main__":
+    node1 = Node(1,0)
+    node2 = Node(2,1)
+    node3 = Node(3,1)
+    node2 = Node(2,1)
+    # node4 = Node(3,3)
