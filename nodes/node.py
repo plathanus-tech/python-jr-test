35fe0d65ffb7ab10eae38523b7eee9ef5b24a0d4
Gustavo Domingos Garcia <81938273+gdomingosg@users.noreply.github.com>
2022-02-14 19:27:56 -0300

    Update remove and removes_cascade methods

--- nodes/node.py
+++ nodes/node.py
@@ -2,4 +2,26 @@
 
 
 class Node:
-    pass
+    id: int
+    parent: int
+
+    def __init__(self, id: int, parent: int):
+
+        if not isinstance(id, int):
+            raise ValueError("The node's id must to be an interger")
+        else:
+            self.id = id
+
+        if not isinstance(parent, int):
+            raise ValueError("The node's parent must to be an interger")
+        elif parent >= id:
+            raise ValueError("An Node parent can't be greater or equal to the node's id")
+        else:
+            self.parent = parent
+
+    def __repr__(self) -> str:
+        return f'Node({self.id}, {self.parent})'
+
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self.id == other.id and self.parent == other.parent
