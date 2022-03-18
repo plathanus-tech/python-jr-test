628aa8de325135d4d3b2ae3656959099b1c63a80
Guido Filho <guido.warken@gmail.com>
2022-03-18 10:00:17 -0300

    feat(Node): Creates the node class according to criteria

--- nodes/node.py
+++ nodes/node.py
@@ -2,4 +2,21 @@
 
 
 class Node:
-    pass
+
+    id: int = 0
+    parent: int = 0
+
+    def __init__(self, id: int, parent: int) -> None:
+
+        if parent >= id:
+            raise ValueError('parent can not to be greater than its id')
+
+        if not type(id) == int or not type(parent) == int:
+            raise ValueError('id and parent must have integer values')
+
+        self.id = id
+        self.parent = parent
+
+    def __repr__(self) -> str:
+        return 'Node(' + str(self.id) + ', ' + str(self.parent) + ')'
+
