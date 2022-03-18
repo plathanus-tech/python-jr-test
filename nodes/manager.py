f7ef82a6d26914269c586f4b3dd3f1b5f1916fee
Guido Filho <guido.warken@gmail.com>
2022-03-18 17:56:09 -0300

    feat(NodeManager): creates the node manager class and completes the chalenge

--- nodes/manager.py
+++ nodes/manager.py
@@ -1,5 +1,52 @@
+from typing import List
+from .node import Node
+
 # Create here the NodeManager Class
 
-
 class NodeManager:
-    pass
+    nodes: List[Node] = []
+
+    def __init__(self, nodes: List[Node]) -> None:
+        if type(nodes) != list:
+            raise ValueError('You must give a list of nodes for creating a node manager. You have passed an argument of type {} with value {}'.format(nodes.__class__.__name__, nodes))
+
+        if len(nodes) == 0:
+            raise ValueError('You must provide a list of nodes with at least 1 element of type Node. The list you have passed is empty')
+
+        for node in nodes:
+            if not isinstance(node, Node):
+                raise ValueError('All elements from listt need to be an instance of Node')
+
+        self.nodes = [n for n in nodes]
+
+    def __len__(self) -> int:
+        return len(self.nodes)
+
+    def __getitem__(self, index: int):
+        if index >= len(self.nodes) or index < 0:
+            raise IndexError('index is out of range')
+
+        return self.nodes[index]
+
+    def remove(self, node: Node) -> None:
+        if not isinstance(node, Node):
+            raise ValueError('You must pass a node to be remove')
+
+        if not node in self:
+            raise ValueError('this node is not present in the manager')
+
+        self.nodes.remove(node)
+
+    def remove_cascade(self, node: Node) -> None:
+        if not isinstance(node, Node):
+            raise ValueError('You must pass a node to be remove')
+
+        if not node in self:
+            raise ValueError('this node is not present in the manager')
+
+        children: List[Node] = [n for n in self.nodes if n.parent == node.id]
+        for child in children:
+            self.remove_cascade(child)
+
+        self.remove(node)
+
--- nodes/node.py
+++ nodes/node.py
@@ -1,15 +1,13 @@
 # Create here the Node Class
 
-
 class Node:
 
     id: int = 0
     parent: int = 0
 
     def __init__(self, id: int, parent: int) -> None:
-
         if parent >= id:
-            raise ValueError('parent can not to be greater than its id')
+            raise ValueError('parent can not be greater than its id')
 
         if not type(id) == int or not type(parent) == int:
             raise ValueError('id and parent must have integer values')
@@ -19,4 +17,10 @@ class Node:
 
     def __repr__(self) -> str:
         return 'Node(' + str(self.id) + ', ' + str(self.parent) + ')'
+
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            return False
+
+        return self.id == other.id and self.parent == other.parent
 
