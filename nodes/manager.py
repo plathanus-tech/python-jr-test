937cd97333a73aee4be0bc3e2387b8247b27ba1c
Junior Bibiano <juniorbibiano@gmail.com>
2022-03-26 11:06:05 -0300

    .

--- nodes/manager.py
+++ nodes/manager.py
@@ -1,5 +1,39 @@
-# Create here the NodeManager Class
+from typing import List
+
+from nodes.node import Node
 
 
 class NodeManager:
-    pass
+
+    def __init__(self, nodes: List[Node]):
+        self.nodes = nodes[:]
+
+    def __len__(self):
+        return self.nodes.__len__()
+
+    def __getitem__(self, item):
+        return self.nodes[item]
+
+    def remove(self, node: Node) -> None:
+        self.__validate_node(node)
+        self.nodes.remove(node)
+
+    def remove_cascade(self, node: Node) -> None:
+        self.__validate_node(node)
+
+        deleted: List[Node] = [node]
+        for node in deleted:
+            for nd in self.nodes:
+                if node.id == nd.parent:
+                    deleted.append(nd)
+            self.__remove_nodes(deleted)
+
+    def __validate_node(self, node: Node) -> None:
+        if node not in self.nodes:
+            raise ValueError()
+
+    def __remove_nodes(self, deleted: List[Node]):
+        for nd in deleted:
+            if nd in self.nodes:
+                self.nodes.remove(nd)
+
--- nodes/node.py
+++ nodes/node.py
@@ -1,5 +1,30 @@
-# Create here the Node Class
-
-
 class Node:
-    pass
+
+    def __init__(self, id: int, parent: int):
+        self.id = id
+        self.parent = parent
+
+    def __repr__(self):
+        return f'node_{self.id - 1}'
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        if not isinstance(value, int):
+            raise ValueError('The node id must be an integer.')
+        self.__id = value
+
+    @property
+    def parent(self):
+        return self.__parent
+
+    @parent.setter
+    def parent(self, value):
+        if not isinstance(value, int):
+            raise ValueError('The node parent must be an integer.')
+        elif value >= self.id:
+            raise ValueError('The node parent must be less than node id.')
+        self.__parent = value
